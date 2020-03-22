919e0942d96617c2fafc6ae79bb918ce4aae35ad
Marinitch Andrii <mar_a@ukr.net>
2020-03-22 17:35:27 +0200

    delete Abstract class

--- Controllers/mainController.php
+++ Controllers/mainController.php
@@ -2,30 +2,9 @@
 
 namespace Controllers;
 
-use Services\Db;
-use Models\News;
-use Models\Author;
 
 class mainController
 {
-//     /** @var View */
-////    private $view;
-//
-//    /** @var Db */
-//    private $db;
-//
-//    public function __construct()
-//    {
-////        $this->view = new View(__DIR__ . '/View');
-//        $this->db = Db::getInstance();
-//    }
-//
-//    public function index()
-//    {
-//        $articles = $this->db->query('SELECT * FROM `news`;');
-//        var_dump($articles);
-//        return;
-//    }
 
 
 }
