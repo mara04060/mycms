f20247a417bd8f74290ba6896b81936bb6f0902c
Marinitch Andrii <mar_a@ukr.net>
2020-03-24 14:29:40 +0200

    Add 404 Page error

--- View/404.php
+++ View/404.php
@@ -1,22 +1,24 @@
 <?php
+$menu = "<a href=\"/\" class=\"blog-nav-item active\">Главная страница</a>
+            <a href=\"/news/\" class=\"blog-nav-item\">News</a>
+            <a href=\"/author/\" class=\"blog-nav-item\">Author</a>";
+
+$small_menu = "<li><a href=\"/\">Главная страница</a></li>
+                    <li><a class=\"active\" href=\"/author/\">Author</a></li>
+                    <li><a href=\"/news/\">News</a></li>";
+
+$title = "Error 404";
+$blog_title = "Error 404";
+$blog_description = "Error 404";
+$javascript = '';
+
+include_once ('indexTemplate.php');
 ?>
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <title>Error <?=$data ?> </title>
-    <link rel="stylesheet" href="/styles.css">
-</head>
-<body>
 
-<table class="layout">
-    <tr>
-        <td colspan="2" class="header">
-            <h1><?=$data?> Страница не найдена</h1>
 
-        </td>
-    </tr>
-
-</table>
-</body>
-</html>
+<div class="col-sm-8 blog-main">
+    <h1>404</h1>
+</div><!-- /.blog-main -->
+<?php
+include_once ('footerTemplate.php');
+?>
